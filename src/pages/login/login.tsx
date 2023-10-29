67918682d739e491fbf8801c4f566696f5dfc3cb
Amushkaa <ameee.am@gmail.com>
2023-10-30 00:09:25 +0300

    Header учитывает путь страницы

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,37 +3,19 @@ import Form from '../../components/form/form';
 
 export default function Login(): JSX.Element {
   return (
-    <div className="page page--gray page--login">
+    <div className='page page--gray page--login'>
       <Helmet>
         <title>Войти в аккаунт</title>
       </Helmet>
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width="81"
-                  height="41"
-                />
-              </a>
-            </div>
-          </div>
-        </div>
-      </header>
-
-      <main className="page__main page__main--login">
-        <div className="page__login-container container">
-          <section className="login">
-            <h1 className="login__title">Sign in</h1>
-            <Form/>
+      <main className='page__main page__main--login'>
+        <div className='page__login-container container'>
+          <section className='login'>
+            <h1 className='login__title'>Sign in</h1>
+            <Form />
           </section>
-          <section className="locations locations--login locations--current">
-            <div className="locations__item">
-              <a className="locations__item-link" href="#">
+          <section className='locations locations--login locations--current'>
+            <div className='locations__item'>
+              <a className='locations__item-link' href='#'>
                 <span>Amsterdam</span>
               </a>
             </div>
